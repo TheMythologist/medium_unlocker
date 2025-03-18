be24ad8358707cf46848d07dcaf6b37934bede4d
TheMythologist <leekaixuan2001@gmail.com>
2025-03-18 11:32:10 +0800

    Persist cookies across sessions

--- components/WebViewComponent.tsx
+++ components/WebViewComponent.tsx
@@ -1,6 +1,9 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import CookieManager, { type Cookies } from '@react-native-cookies/cookies';
 // eslint-disable-next-line import/no-named-as-default
 import Constants from 'expo-constants';
-import { Dimensions, StyleSheet } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { Dimensions, StyleSheet, View, ActivityIndicator } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 const { height, width } = Dimensions.get('screen');
@@ -9,8 +12,59 @@ interface WebViewComponentProps {
   uri: string;
 }
 
+const COOKIE_STORAGE_KEY = 'persistedCookies';
+const SITE_URL = 'https://freedium.cfd';
+
 export default function WebViewComponent({ uri }: WebViewComponentProps) {
-  return <WebView style={styles.container} source={{ uri: ' https://freedium.cfd/' + uri }} />;
+  const webviewRef = useRef(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const restoreCookies = async () => {
+      const savedCookies = await AsyncStorage.getItem(COOKIE_STORAGE_KEY);
+      if (savedCookies) {
+        const parsedCookies: Cookies = JSON.parse(savedCookies);
+        await Promise.all(
+          Object.entries(parsedCookies).map(([cookieName, cookie]) =>
+            CookieManager.set(SITE_URL, {
+              name: cookieName,
+              value: cookie.value,
+              domain: cookie.domain,
+              path: cookie.path || '/',
+              version: '1',
+              expires: cookie.expires || undefined,
+            }),
+          ),
+        );
+        console.log('Cookies restored!');
+      }
+      setLoading(false);
+    };
+    restoreCookies();
+  }, []);
+
+  const saveCookies = async () => {
+    const cookies = await CookieManager.get(SITE_URL);
+    await AsyncStorage.setItem(COOKIE_STORAGE_KEY, JSON.stringify(cookies));
+    console.log('Cookies saved:', cookies);
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      {loading ? (
+        <ActivityIndicator size="large" style={{ flex: 1 }} />
+      ) : (
+        <WebView
+          ref={webviewRef}
+          style={styles.container}
+          source={{ uri: ' https://freedium.cfd/' + uri }}
+          sharedCookiesEnabled={true}
+          thirdPartyCookiesEnabled={true}
+          onNavigationStateChange={saveCookies}
+        />
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
