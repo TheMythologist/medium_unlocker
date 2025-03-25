a50a77660e4c4910ae3eb75687040d70382117d8
TheMythologist <leekaixuan2001@gmail.com>
2025-03-25 10:36:27 +0800

    ui: add loading bar for webview component

--- components/WebViewComponent.tsx
+++ components/WebViewComponent.tsx
@@ -1,15 +1,10 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import CookieManager, { type Cookies } from '@react-native-cookies/cookies';
 import { useContext, useEffect, useRef, useState } from 'react';
-import {
-  ActivityIndicator,
-  BackHandler,
-  Dimensions,
-  Platform,
-  StyleSheet,
-  View,
-} from 'react-native';
+import { BackHandler, Dimensions, Platform, StyleSheet, View } from 'react-native';
+import * as Progress from 'react-native-progress';
 import { WebView, WebViewNavigation } from 'react-native-webview';
+import { WebViewProgressEvent } from 'react-native-webview/lib/WebViewTypes';
 import { CurrentUrlContext } from '@/hooks/useCurrentUrlContext';
 
 const { height, width } = Dimensions.get('screen');
@@ -24,7 +19,9 @@ interface WebViewComponentProps {
 export default function WebViewComponent({ uri }: WebViewComponentProps) {
   const webViewRef = useRef<WebView>(null);
   const canGoBackRef = useRef(false);
+  const timeoutRef = useRef<NodeJS.Timeout>();
   const [loading, setLoading] = useState(true);
+  const [percentageLoaded, setPercentageLoaded] = useState(0);
 
   const [_, setCurrentUrl] = useContext(CurrentUrlContext);
 
@@ -72,21 +69,45 @@ export default function WebViewComponent({ uri }: WebViewComponentProps) {
     await AsyncStorage.setItem(COOKIE_STORAGE_KEY, JSON.stringify(cookies));
   };
 
+  const onLoadProgress = (event: WebViewProgressEvent) => {
+    canGoBackRef.current = event.nativeEvent.canGoBack;
+    setPercentageLoaded(event.nativeEvent.progress);
+  };
+
+  const onLoadStart = () => {
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    timeoutRef.current = undefined;
+    setLoading(true);
+  };
+
+  const onLoadEnd = () => {
+    const timeout = setTimeout(() => setLoading(false), 500);
+    timeoutRef.current = timeout;
+  };
+
   return (
     <View style={{ flex: 1 }}>
-      {loading ? (
-        <ActivityIndicator size="large" style={{ flex: 1 }} />
-      ) : (
-        <WebView
-          ref={webViewRef}
-          style={styles.container}
-          source={{ uri: `${SITE_URL}/${uri}` }}
-          originWhitelist={[SITE_URL]}
-          sharedCookiesEnabled={true}
-          thirdPartyCookiesEnabled={true}
-          onNavigationStateChange={onNavigationStateChange}
-          allowsBackForwardNavigationGestures
-          onLoadProgress={(event) => (canGoBackRef.current = event.nativeEvent.canGoBack)}
+      <WebView
+        ref={webViewRef}
+        style={styles.container}
+        source={{ uri: `${SITE_URL}/${uri}` }}
+        originWhitelist={[SITE_URL]}
+        sharedCookiesEnabled={true}
+        thirdPartyCookiesEnabled={true}
+        onNavigationStateChange={onNavigationStateChange}
+        allowsBackForwardNavigationGestures
+        onLoadStart={onLoadStart}
+        onLoadEnd={onLoadEnd}
+        onLoadProgress={onLoadProgress}
+      />
+      {loading && (
+        <Progress.Bar
+          style={styles.progressBar}
+          progress={percentageLoaded}
+          height={3}
+          borderRadius={0}
+          borderWidth={0}
+          width={width}
         />
       )}
     </View>
@@ -99,5 +120,9 @@ const styles = StyleSheet.create({
     height,
     width,
     overflow: 'hidden',
+  },
+  progressBar: {
+    position: 'absolute',
+    top: 10,
   },
 });
